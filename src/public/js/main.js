0ad5e1002bf8f0dd99b981466174fa419f148526
Brandddd <b.palacio1@utp.edu.co>
2022-11-15 22:44:26 -0500

    Conteo de usuarios

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -6,6 +6,37 @@ $(function () {
   const $messageBox = $("#message");
   const $chat = $("#chat");
 
+  // Obtención de elementos desde el nickname form
+  const $nickForm = $("#nick-form");
+  const $nickError = $("#nick-error");
+  const $nickname = $("#nickname");
+
+  // Obtención del elemento usernames
+  const $usernames = $("#usernames");
+
+  // Capturando los eventos:
+  $nickForm.submit((e) => {
+    e.preventDefault();
+
+    // Escuchando como ingresa un nuevo usuario con web sockets:
+    socket.emit("new user", $nickname.val(), (data) => {
+      // Si data is true:
+      if (data) {
+        $("#nick-wrap").hide();
+        $("#content-wrap").show();
+        // Si no, muestra el error de que el usuario ya está tomado
+      } else {
+        $nickError.html(`
+          <div class="alert alert-danger">
+            Este usuario ya existe
+          </div>
+        `);
+      }
+      // Limpiamos el input
+      $nickname.val("");
+    });
+  });
+
   // Capturando los eventos:
   $messageForm.submit((e) => {
     e.preventDefault();
@@ -14,9 +45,20 @@ $(function () {
     socket.emit("send message", $messageBox.val());
     $messageBox.val("");
   });
-  
+
   // Escuchar evento que viene del servidor sockets y mostrandolo en el chat
   socket.on("new message", function (data) {
-    $chat.append(data + '</br>');
+    $chat.append(
+      `<b><i class="fa-regular fa-comment-dots"></i> ${data.nick}</b>: ${data.msg} <br/>`
+    );
+  });
+
+  // Escucha evento de envio de nicknames
+  socket.on("usernames", (data) => {
+    let html = ""; // Variable para almacenar todos los usuarios
+    for (let i = 0; i < data.length; i++) {
+      html += `<p><i class="fas fa-user"></i> ${data[i]}</p>`; // Recorriendo arreglo para mostrarlo
+    }
+    $usernames.html(html); // Se añaden las etiquetas p para mostrarlas por pantalla
   });
 });
